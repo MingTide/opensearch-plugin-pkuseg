6f5721182225942fca33dce2c7a0c3c60d210f22
sunmingtao <sunmingtao@medaxis.cn>
2024-12-27 11:06:02 +0800

    first commit

---
+++ src/main/java/com/medaxis/pkuseg/plugin/JiebaDict.java
@@ -0,0 +1,39 @@
+package com.medaxis.pkuseg.plugin;
+
+
+import com.medaxis.analysis.jieba.WordDictionary;
+import org.opensearch.env.Environment;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class JiebaDict {
+
+    private static JiebaDict singleton;
+
+    public static JiebaDict init(Environment environment) {
+        if (singleton == null) {
+            synchronized (JiebaDict.class) {
+                if (singleton == null) {
+
+                    TimerTask task = new TimerTask() {
+                        @Override
+                        public void run() {
+                            System.out.println("start to load new dict");
+                            WordDictionary.getInstance()
+                                    .init(environment.pluginsFile().resolve("jieba/dic").toFile());
+                        }
+                    };
+
+                    Timer t = new Timer();
+
+                    t.scheduleAtFixedRate(task, 1000, 60 * 1000);
+
+                    singleton = new JiebaDict();
+                    return singleton;
+                }
+            }
+        }
+        return singleton;
+    }
+}
